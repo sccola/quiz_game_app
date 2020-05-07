0607bca10800f01eef2d2350f19bf214887fe458
sccola <sccola2020@gmail.com>
2020-05-07 04:14:19 +0100

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// Questions' Variables
+let quizRulesDiv = document.querySelector('div.rules-div');
+let questionDiv = document.querySelector('div.question-div');
+let question = document.createElement('p');
+let scoreDiv = document.querySelector('#score-div');
+let startBtn = document.querySelector('button#btn-start');
+let nextBtn = document.querySelector('button#btn-next');
+let playAgainBtn = document.querySelector('#btn-restart');
+
+
 
